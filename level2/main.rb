03e23ab480f5a8fa1c92cd0784e7ae481d86da36
PiRB <pierrebrivio14@gmail.com>
2024-01-02 10:38:16 +0100

    refacto : level2

--- level2/main.rb
+++ level2/main.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require 'json'
 require 'date'
+require '../lib/rent_days'
 
 INPUT_FILE_PATH = './data/input.json'
 OUTPUT_FILE_PATH = './data/output.json'
@@ -9,46 +10,55 @@ INPUT_FILE = File.read(INPUT_FILE_PATH)
 File.open(OUTPUT_FILE_PATH, 'w')
 HASH_DATA = JSON.parse(INPUT_FILE)
 
-def compute_rent_days(rent:)
-  start_date = Date.parse(rent['start_date'])
-  end_date =  Date.parse(rent['end_date'])
+DISCOUNT_AFTER_1_DAY = 0.9
+DISCOUNT_AFTER_4_DAYS = 0.7
+DISCOUNT_AFTER_10_DAYS = 0.5
+MAXIMUM_DAYS_WITH_10_PERCENT_DISCOUNT = 3
+MAXIMUM_DAYS_WITH_30_PERCENT_DISCOUNT = 6
 
-  return -1 if start_date > end_date
+def days_rent_price(rent:, price_per_day:)
+  rent_days = compute_rent_days(rent)
+  case rent_days
+  when 5..10
+    remaining_days = rent_days - 4
+    rent_days_price = price_per_day + 
+                      MAXIMUM_DAYS_WITH_10_PERCENT_DISCOUNT * price_per_day * DISCOUNT_AFTER_1_DAY + 
+                      remaining_days * price_per_day * DISCOUNT_AFTER_4_DAYS
+  when 2..4
+    remaining_days = rent_days - 1
+    rent_days_price = price_per_day + remaining_days * price_per_day * DISCOUNT_AFTER_1_DAY
+  when 1
+    rent_days_price = price_per_day 
+  else
+    remaining_days = rent_days - 10
+    rent_days_price = price_per_day + 
+                      MAXIMUM_DAYS_WITH_10_PERCENT_DISCOUNT * price_per_day * DISCOUNT_AFTER_1_DAY + 
+                      MAXIMUM_DAYS_WITH_30_PERCENT_DISCOUNT * price_per_day * DISCOUNT_AFTER_4_DAYS + 
+                      remaining_days * price_per_day * DISCOUNT_AFTER_10_DAYS
+  end
 
-  return end_date - start_date + 1
+  rent_days_price
+end
+
+def distance_rent_price(distance:, price_per_km:)
+  distance * price_per_km
 end
 
 def compute_rent_pricing(car:, rent:)
-  return 0 if compute_rent_days(rent:) == 0
-  rent_days = compute_rent_days(rent:)
-  case rent_days
-  when 5..10
-    rent_price = car['price_per_day'] + 3 * car['price_per_day'] * 0.9 + (rent_days - 4) * car['price_per_day'] * 0.7
-  when 2..4
-    rent_price = car['price_per_day'] + (rent_days - 1) * car['price_per_day'] * 0.9
-  when 1
-    rent_price = car['price_per_day'] 
-  else
-    rent_price = car['price_per_day'] + 3 * car['price_per_day'] * 0.9 + 6 * car['price_per_day'] * 0.7 + (rent_days - 10) * car['price_per_day'] * 0.5
-  end
+  days_price = days_rent_price(rent:, price_per_day: car['price_per_day'])
+  distance_price = distance_rent_price(distance: rent['distance'], price_per_km: car['price_per_km'])
 
-  rent_price += rent['distance'] * car['price_per_km'].to_i
+  days_price + distance_price
 end
 
-def compute_rentals
+def generate_rentals_hash
   HASH_DATA['rentals'].map do |rent|
     car = HASH_DATA['cars'].find { |car| car['id'] == rent['car_id'] }
 
     rent_price = compute_rent_pricing(car:, rent:)
 
-    { "id": rent['id'], "price": rent_price}
+    { "id": rent['id'], "price": rent_price }
   end
 end
 
-def output_data
-  {
-    "rentals": compute_rentals
-  }
-end
-
-File.open(OUTPUT_FILE_PATH, 'w') {|file| file.write(JSON.pretty_generate(output_data))}
+File.open(OUTPUT_FILE_PATH, 'w') {|file| file.write(JSON.pretty_generate({ "rentals": generate_rentals_hash }))}
--- level2/spec/main_spec.rb
+++ level2/spec/main_spec.rb
@@ -1,4 +1,5 @@
-require "./main"
+require './main'
+require '../lib/rent_days'
 
 describe '#compute_rent_days' do
   context 'with a rent for a day' do
@@ -12,7 +13,7 @@ describe '#compute_rent_days' do
       } 
     }
     it 'should return 1' do
-      expect(compute_rent_days(rent: rent)).to eq(1)
+      expect(compute_rent_days(rent)).to eq(1)
     end
   end
 
@@ -27,7 +28,7 @@ describe '#compute_rent_days' do
       } 
     }
     it 'should return 2' do
-      expect(compute_rent_days(rent: rent)).to eq(2)
+      expect(compute_rent_days(rent)).to eq(2)
     end
   end
 
@@ -42,7 +43,7 @@ describe '#compute_rent_days' do
       } 
     }
     it 'should return 3' do
-      expect(compute_rent_days(rent: rent)).to eq(3)
+      expect(compute_rent_days(rent)).to eq(3)
     end
   end
 
@@ -57,101 +58,83 @@ describe '#compute_rent_days' do
       } 
     }
     it 'should return 10' do
-      expect(compute_rent_days(rent: rent)).to eq(10)
+      expect(compute_rent_days(rent)).to eq(10)
     end
   end
+end
 
-  context 'with a start date greater than the end date' do
+describe '#days_rent_price' do
+  context ' with a rent for a day and a price of 1000 per day' do
     let(:rent) { 
       {
         "id"=> 1,
         "car_id"=> 1,
         "start_date"=> "2017-12-8",
-        "end_date"=> "2017-12-7",
+        "end_date"=> "2017-12-8",
         "distance"=> 100
       } 
     }
-    it 'should return -1' do
-      expect(compute_rent_days(rent: rent)).to eq(-1)
-    end
-  end
-end
-
-describe '#compute_rent_pricing' do
-  context ' with a rent for a day' do
-    let (:car) {
-      { "id"=> 1, "price_per_day"=> 1000, "price_per_km"=> 10 }
-    }
-    let (:rent_days) { 1 }
-    let (:rent) { 
-      {
-        "id"=> 1,
-        "car_id"=> 1,
-        "start_date"=> "2017-12-8",
-        "end_date"=> "2017-12-8",
-        "distance"=> 10
-      }
-    }
-
-    it 'should return 1100' do
-      expect(compute_rent_pricing(car:, rent:)).to eq(1100)
+    let(:price_per_day) { 1000 }
+    it ' should renturn 1000' do
+      expect(days_rent_price(rent:, price_per_day:)).to eq(1000)
     end
   end
 
-  context ' with a rent for 3 days' do
-    let (:car) {
-      { "id"=> 1, "price_per_day"=> 1000, "price_per_km"=> 10 }
-    }
-    let (:rent) { 
+  context ' with a rent for three days and a price of 750 per day' do
+    let(:rent) { 
       {
         "id"=> 1,
         "car_id"=> 1,
         "start_date"=> "2017-12-8",
         "end_date"=> "2017-12-10",
-        "distance"=> 10
-      }
+        "distance"=> 100
+      } 
     }
-
-    it 'should return 2900' do
-      expect(compute_rent_pricing(car:, rent:)).to eq(2900)
+    let(:price_per_day) { 750 }
+    it ' should renturn 2100' do
+      expect(days_rent_price(rent:, price_per_day:)).to eq(2100)
     end
   end
 
-  context ' with a rent for 10 days' do
-    let (:car) {
-      { "id"=> 1, "price_per_day"=> 1000, "price_per_km"=> 10 }
-    }
-    let (:rent) { 
+  context ' with a rent for ten days and a price of 1000 per day' do
+    let(:rent) { 
       {
         "id"=> 1,
         "car_id"=> 1,
         "start_date"=> "2017-12-8",
         "end_date"=> "2017-12-17",
-        "distance"=> 10
-      }
+        "distance"=> 100
+      } 
     }
+    let(:price_per_day) { 1000 }
+    it ' should renturn 7900' do
+      expect(days_rent_price(rent:, price_per_day:)).to eq(7900)
+    end
+  end
+end
 
-    it 'should return 8000' do
-      expect(compute_rent_pricing(car:, rent:)).to eq(8000)
+describe '#distance_rent_price' do
+  context ' with 100 km and a price of 10 per km' do
+    let(:distance) { 100 }
+    let(:price_per_km) { 10 }
+    it ' should return 1000' do
+      expect(distance_rent_price(distance:, price_per_km:)).to eq(1000)
     end
   end
 
-  context ' with a rent for 20 days' do
-    let (:car) {
-      { "id"=> 1, "price_per_day"=> 1000, "price_per_km"=> 10 }
-    }
-    let (:rent) { 
-      {
-        "id"=> 1,
-        "car_id"=> 1,
-        "start_date"=> "2017-12-8",
-        "end_date"=> "2017-12-27",
-        "distance"=> 10
-      }
-    }
+  context ' with 200 km and a price of 8 per km' do
+    let(:distance) { 200 }
+    let(:price_per_km) { 8 }
+    it ' should return 1600' do
+      expect(distance_rent_price(distance:, price_per_km:)).to eq(1600)
+    end
+  end
 
-    it 'should return 13000' do
-      expect(compute_rent_pricing(car:, rent:)).to eq(13000)
+  context ' with 1000 km and a price of 12 per km' do
+    let(:distance) { 1000 }
+    let(:price_per_km) { 12 }
+    it ' should return 12000' do
+      expect(distance_rent_price(distance:, price_per_km:)).to eq(12000)
     end
   end
 end
